25df77af0815ceb3935cec18ad910d1a00f369bf
chrishargan <chrishargan@gmail.com>
2020-11-20 15:51:03 +0100

    Using styled components library and adding FAQ and services sections

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import home1 from '../img/home1.png';
+// Styled 
+import styled from 'styled-components';
 
 const AboutSection = () => {
     return (
-        <div className="description">
-            <div className="title">
-                <div className="hide">
-                    <h2>I love to create</h2>
+        <StyledAbout>
+            <StyledDescription>
+                <div className="title">
+                    <div className="hide">
+                        <h2>I make</h2>
+                    </div>
+                    <div className="hide">
+                        <h2>Artworks <span>WebDev</span> & </h2>
+                    </div>
+                    <div className="hide">
+                        <h2>Fashion.</h2>
+                    </div>
                 </div>
-                <div className="hide">
-                    <h2>Artworks <span>Web-Development</span> & </h2>
-                </div>
-                <div className="hide">
-                    <h2>Fashion</h2>
-                </div>
-            </div>
-            <p>Get in contact and let's make great things</p>
-            <button>Contacts Us</button>
-            <div className="image">
-                <img src={home1} alt="profile" />
-            </div>
-        </div>
-    )
+                <p>Enjoy the site and feel free to get in touch</p>
+                <button>Contacts Us</button>
+            </StyledDescription>
+            <img src={home1} alt="profile" />
+        </StyledAbout>
+    );
+};
+
+//Styled Component
+const StyledAbout = styled.div`
+min-height: 90vh;
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 5rem 10rem;
+color: white;
+`;
+const StyledDescription = styled.div`
+flex: 1;
+padding-right: 5rem;
+h2{
+    font-weight: lighter;
 }
+`;
+
 
 export default AboutSection;
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,9 +1,10 @@
 import React from 'react';
-// import Icons
+// import Icons and images
 import clock from '../img/clock.svg';
 import diaphragm from '../img/diaphragm.svg';
 import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
+import home2 from '../img/home2.png';
 
 const ServicesSection = () => {
     return (
@@ -16,31 +17,37 @@ const ServicesSection = () => {
                             <img src={clock} alt="clock" />
                             <h3>Deadline Driven</h3>
                         </div>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </div>
                     <div className="card">
                         <div className="icon">
                             <img src={diaphragm} alt="diaphragm" />
                             <h3>Eye for Detail</h3>
                         </div>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. </p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </div>
                     <div className="card">
                         <div className="icon">
                             <img src={money} alt="money" />
                             <h3>Open to Commissions</h3>
                         </div>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </div>
                     <div className="card">
                         <div className="icon">
                             <img src={teamwork} alt="teamwork" />
                             <h3>Part of the Team</h3>
                         </div>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </div>
 
                 </div>
             </div>
+            <div className="image">
+                <img src={home2} alt="Homepage" />
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default ServicesSection;
